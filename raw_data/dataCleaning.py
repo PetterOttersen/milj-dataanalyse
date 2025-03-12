e603f9ebd1d09afb50cda84130054da6e7a17e65
Einar Bones <einarbnes@gmail.com>
2025-03-12 10:57:26 +0100

    Did some work on dataCleaning.py, + added xlsx file for easier readability

--- raw_data/dataCleaning.py
+++ raw_data/dataCleaning.py
@@ -3,3 +3,6 @@ from getData import df
 #Vi behandler dataframen sånn at vi kun får de dataene vi synes er relevante
 
 df.to_excel("Excelfil.xlsx")
+
+df_trimmed = df[["index", "elementId", "value", "timeOffset", "qualityCode", "sourceId"]]
+
