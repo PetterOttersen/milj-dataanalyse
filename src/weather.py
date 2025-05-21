d16845175658ef48e0839edd7707fd7c6ef59b7e
Petter <paotters@stud.ntnu.no>
2025-05-21 18:39:27 +0200

    endrer funskjonsnavn

--- src/weather.py
+++ src/weather.py
@@ -12,7 +12,7 @@ class analyse_og_visualisere:
     def __init__(self, file_path="../raw_data/data/Vaerdata.csv"):
         self.FILE_PATH = file_path
 
-    def analyze_weather_data(self):
+    def analyser_værdata_data(self):
     
     # Leser data fra Excel-filen
         data = pd.read_csv(self.FILE_PATH)
